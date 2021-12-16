b6e4705875ddf4829bc3633eeb63b68b9e760dcf
koresachin7 <koresachin7@gmail.com>
2021-12-16 12:19:44 +0530

    Added : Adding framework of program

---
+++ addressbook.py
@@ -0,0 +1,32 @@
+"""
+* @Author: Sachin S Kore
+* @Date: 2021-12-14
+* @Title : Adding Address Book Data
+"""
+
+
+
+
+class AddressBook:
+
+    def __init__(self, list):
+        self.list = list
+
+    def remove(self):
+        pass
+
+    def update(self):
+        pass
+
+    def display(self):
+        pass
+
+
+class Contact:
+
+    def __init__(self):
+        pass
+
+
+if __name__ == '__main__':
+   print("Welcome To Address Book")
